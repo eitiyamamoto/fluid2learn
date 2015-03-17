3d63d540b3e96b840ae92007193d518dc68f4886
Vkenji <vitor95uema@hotmail.com>
2015-03-16 22:01:41 -0300

    ajustes finais

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -1,6 +1,5 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
-// import java.util.Scanner;
 
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
@@ -76,33 +75,11 @@ public class EnquirerMaze implements IEnquirer {
 				}	
 			}
 		}
-		
-		/*Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-		String tipo = scanner.nextLine();
-		while (!tipo.equalsIgnoreCase("F")) {
-		   System.out.print("  --> ");
-		   String pc = scanner.nextLine();
-		   switch (tipo.toUpperCase()) {
-		      case "P": String resposta = responder.ask(pc);
-		                System.out.println("  Resposta: " + resposta);
-		                break;
-		      case "M": boolean moveu = responder.move(pc);
-		                System.out.println((moveu)?"  Movimento executado!":"N�o � poss�vel mover");
-		                break;
-		   }
-			System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-			tipo = scanner.nextLine();
-		}*/
-		
 
 		if (responder.finalAnswer("cheguei"))
 			System.out.println("Voce encontrou a saida!");
 		else
 			System.out.println("Fuem fuem fuem!");
-		
-		// scanner.close();
 		
 		return true;
 	}
